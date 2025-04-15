95b3bf4076afb6006861b18b18b6c9707b53a364
Chane Du Toit <chanedt1008@gmail.com>
2025-04-15 16:28:37 +0200

    Implement player movement controls and input handling

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,2 +1,22 @@
 extends CharacterBody2D
 
+var speed = 300
+
+func _physics_process(delta: float) -> void:
+	# Move the player based on input
+	velocity = Vector2.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+
+	move_and_slide()
+
+	var screen_size = get_viewport().size
+	# Make sure player isn't able to exit game window
+	global_position = global_position.clamp(Vector2.ZERO, screen_size)
