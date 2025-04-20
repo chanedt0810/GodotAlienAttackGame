1767195b97442a82c6c72372b528a06c1c4d8b60
Chane Du Toit <chanedt1008@gmail.com>
2025-04-20 20:42:29 +0200

    Add path enemy functionality: implement path enemy spawning and movement logic; connect signals for path enemy events

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,18 +1,25 @@
 extends Node2D
 
 signal enemy_spawned(enemy_instance: Node)
+signal path_enemy_spawned(path_enemy_instance: Node)
 
 var enemy_scene = preload("res://scenes/enemy.tscn")
+var path_enemy_scene = preload("res://scenes/path_enemy.tscn")
 
 @onready var timer = $Timer
+@onready var path_enemy_timer = $PathEnemyTimer
 @onready var spawn_positions = $SpawnPositions
 
 func _ready() -> void:
 	# Connect the signal to the function
 	timer.connect("timeout", _on_timer_timeout)
+	path_enemy_timer.connect("timeout", _on_path_enemy_timer_timeout)
 
 func _on_timer_timeout() -> void:
 	spawn_enemy()
+
+func _on_path_enemy_timer_timeout() -> void:
+	spawn_path_enemy()
 	
 func spawn_enemy():
 	# Get a random spawn position
@@ -23,3 +30,8 @@ func spawn_enemy():
 	enemy_instance.global_position = random_spawn_position.global_position
 	# Add the enemy instance to the scene
 	enemy_spawned.emit(enemy_instance)
+
+func spawn_path_enemy():
+	# Get path enemy instance
+	var path_enemy_instance = path_enemy_scene.instantiate()
+	emit_signal("path_enemy_spawned", path_enemy_instance)
--- scripts/game.gd
+++ scripts/game.gd
@@ -17,6 +17,7 @@ func _ready() -> void:
 	death_zone.connect("area_entered", _on_death_zone_area_entered)
 	player.connect("took_damage", _on_player_took_damage)
 	enemy_spawner.connect("enemy_spawned", _on_enemy_spawner_enemy_spawned)
+	enemy_spawner.connect("path_enemy_spawned", _on_enemy_spawner_path_enemy_spawned)
 	hud.set_score_label(score)
 	hud.set_lives_left_label(lives)
 
@@ -42,6 +43,11 @@ func _on_enemy_spawner_enemy_spawned(enemy_instance: Node) -> void:
 	enemy_instance.connect("died", _on_enemy_died)
 	# Add the enemy instance to the scene
 	add_child(enemy_instance)
+
+func _on_enemy_spawner_path_enemy_spawned(path_enemy_instance: Node) -> void:
+	# Add the path enemy instance to the scene
+	add_child(path_enemy_instance)
+	path_enemy_instance.enemy.connect("died", _on_enemy_died)
 
 func _on_enemy_died() -> void:
 	# Increase score when an enemy dies	
